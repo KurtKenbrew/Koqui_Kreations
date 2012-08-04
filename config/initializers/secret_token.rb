8339c7323454f42637409b5c5758cf96cb01b723
kurtkenebrew <kurtkenebrew@fan.(none)>
2012-08-04 00:39:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoquiKreations::Application.config.secret_token = '1a2767cd11c146427cb2209aa32c7f961e9c2664f081f4fc5cd7c2983296797127ec16f70696637e80c4d0fa3ef6642b4c98c1c90438f08540cb492b441574b7'
